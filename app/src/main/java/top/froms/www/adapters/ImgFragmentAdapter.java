357e778f8f91dafdbc48409aabb42d24b05b0e90
yanchao@cnpc.com.cn <yanchao@cnpc.com.cn>
2019-06-20 18:07:42 +0800

    删除一个

--- app/src/main/java/top/froms/www/adapters/ImgFragmentAdapter.java
+++ app/src/main/java/top/froms/www/adapters/ImgFragmentAdapter.java
@@ -13,17 +13,6 @@ import com.bumptech.glide.Glide;
 import top.froms.www.R;
 import top.froms.www.adapters.ImgFragmentAdapter.ImgHolder;
 import top.froms.www.repo.ImgData;
-
-/**
- * 北京中油瑞飞信息技术有限责任公司 研究院 瑞信项目All Rights Reserved
- *
- * @author : yanchao
- * @version ：
- * @project : 瑞信项目
- * @ClassName:ImgFragmentAdapter
- * @Description:
- * @date : 2019-06-20 13:03
- */
 public class ImgFragmentAdapter extends PagedListAdapter<ImgData, ImgHolder> {
 
     public ImgFragmentAdapter(
